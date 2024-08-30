0d3df213ba58fd9ca3c0c39f8b8ee7652310be76
Morell Alargha <m.argha@web.de>
2024-08-30 12:14:28 +0200

    non working changes

--- dev-test.js
+++ dev-test.js
@@ -1,10 +1,15 @@
 // Import
 const Block = require("./block");
+const Blockchain = require("./blockchain");
 
 /***  Test der Block-Funktionalität  */
 // const block = new Block("aktuelle zeit","hash1","hash2","Blockdaten");
 // console.log(block.toString());
 
 //console.log(Block.genesis().toString());
-const testBlock = Block.mineBlock(Block.genesis(), "testDaten");
-console.log(testBlock.toString());
+// const testBlock = Block.mineBlock(Block.genesis(), "testDaten");
+// console.log(testBlock.toString());
+
+/***  Test der Blockchain-Funktionalität  */
+const chain = new Blockchain();
+console.log(chain);
